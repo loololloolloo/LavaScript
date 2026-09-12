3fe90a4322949ff03e812c39229de54cb358da2f
loololloolloo <20055350+68@students.taylorschools.net>
2026-09-12 13:27:03 -0400

    Add first reconstructed browser world geometry

---
+++ vortex-wasm/src/world.js
@@ -0,0 +1,9 @@
+export const WORLD={
+  groundY:0,
+  blocks:[
+    {x:-7,y:1,z:-5,w:3,h:2,d:3},
+    {x:6,y:1,z:-3,w:4,h:2,d:2},
+    {x:-3,y:0.5,z:6,w:6,h:1,d:2},
+    {x:7,y:1.5,z:6,w:2,h:3,d:2}
+  ]
+};
